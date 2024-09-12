2beef2e93e479885cb36dbf096a00aa9d2e170f3
JustsPuspurs <144904136+JustsPuspurs@users.noreply.github.com>
2024-09-12 11:19:23 +0300

    neszuknu
    12345678io

--- backend/resources/js/app.jsx
+++ backend/resources/js/app.jsx
@@ -1,3 +1,5 @@
+import './bootstrap';
+
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 
@@ -9,5 +11,4 @@ createInertiaApp({
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
   },
-  
 })
