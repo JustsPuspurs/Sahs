f9a4370758aafd8b2a3592f943f59a55d763e47f
JustsPuspurs <144904136+JustsPuspurs@users.noreply.github.com>
2024-09-17 13:12:39 +0300

    asd
    asd

---
+++ laravel/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            refresh: true,        
+        }),
+    ],
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
+        return pages[`./Pages/${name}.jsx`]
+    },  
+    
+});
